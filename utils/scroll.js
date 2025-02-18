2ccb5ced7ef38ca2e46f67638814915773492dd4
yusuf <yusu@duck.com>
2025-02-19 00:09:42 +0300

    feat(helpers/scroll): f

---
+++ utils/scroll.js
@@ -0,0 +1,11 @@
+export const scroll = async (page, div) => {
+    await page.evaluate(async (div) => {
+        let lastScrollTop = -1;
+
+        while (div.scrollTop !== lastScrollTop) {
+            lastScrollTop = div.scrollTop;
+            div.scrollTop += div.clientHeight * 2;
+            await new Promise(resolve => setTimeout(resolve, 2000)); // maybe you should increase the delay
+        }
+    }, div);
+}
