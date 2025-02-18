92db99266e07486b1388454214c2c2da5ecf8885
yusuf <yusu@duck.com>
2025-02-19 00:36:49 +0300

    feat(write): write data to file

---
+++ middlewares/write.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+
+const write = async (data, message) => {
+
+    message('Writing followers data to file...');
+    await fs.writeFileSync('dist/followers.json', JSON.stringify(data.followers, null, 4));
+
+    message('Writing following data to file...');
+    await fs.writeFileSync('dist/following.json', JSON.stringify(data.following, null, 4));
+}
+
+module.exports = write;
+g
