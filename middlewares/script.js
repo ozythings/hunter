6c02861424e5fc9f65c3aaef1ab01018f86177b1
yusuf <yusu@duck.com>
2025-02-18 23:06:52 +0300

    fix(script): fix program path

--- middlewares/script.js
+++ middlewares/script.js
@@ -1,3 +1,15 @@
-export default function script(puppeteer, session, target) {
+export default async function script(puppeteer, session, target, message, sleep) {
+    message('Launching browser...');
+    const browser = await puppeteer.launch({
+        headless: false, // visible browser
+        defaultViewport: null,
+        executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", // your chrome path
+        args: ['--start-maximized', '--window-size=1920,1080', '--no-sandbox', '--disable-setuid-sandbox'],
+    });
+
+    message('Opening new page...');
+    const page = await browser.newPage();
+
+    await page.goto(target);
 
 }
