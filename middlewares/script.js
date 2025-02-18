16e921af135761118eafebce907f0c96584b9b57
yusuf <yusu@duck.com>
2025-02-19 01:08:20 +0300

    style(script): invisible browser

--- middlewares/script.js
+++ middlewares/script.js
@@ -7,7 +7,7 @@ import scroll from '../utils/scroll.js';
 export default async function script(puppeteer, session, target, message, sleep) {
     message('Launching browser...');
     const browser = await puppeteer.launch({
-        headless: false, // visible browser
+        headless: true, // invisible browser
         defaultViewport: null,
         executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", // your chrome path
         args: ['--start-maximized', '--window-size=1920,1080', '--no-sandbox', '--disable-setuid-sandbox'],
