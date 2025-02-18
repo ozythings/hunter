23fdb08ab095cad2f419463fadd7558ccccd3e79
yusuf <yusu@duck.com>
2025-02-19 01:07:18 +0300

    feat(write): change path

--- middlewares/write.js
+++ middlewares/write.js
@@ -1,12 +1,18 @@
 import fs from 'fs';
 
-const write = async (data, message) => {
+const write = async (data, message, user, date) => {
+    const f = date();
+    const d = f.split(' ')[0].split('.');
+    const t = f.split(' ')[1].split(':');
+
+    message('Creating folders...');
+    await fs.mkdirSync(`dist/${user}/${d[1] + d[0] + d[2]}/${t[0]}`, {recursive: true});
 
     message('Writing followers data to file...');
-    await fs.writeFileSync('dist/followers.json', JSON.stringify(data.followers, null, 4));
+    await fs.writeFileSync(`dist/${user}/${d[1] + d[0] + d[2]}/${t[0]}/${t[1]}_followers.json`, JSON.stringify(data.followers, null, 4));
 
     message('Writing following data to file...');
-    await fs.writeFileSync('dist/following.json', JSON.stringify(data.following, null, 4));
+    await fs.writeFileSync(`dist/${user}/${d[1] + d[0] + d[2]}/${t[0]}/${t[1]}_following.json`, JSON.stringify(data.following, null, 4));
 }
 
 export default write;
