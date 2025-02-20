27eff98e0812ff63d53f422900c41cc7dce453f1
ozythings <54993448+ozythings@users.noreply.github.com>
2025-02-21 02:39:08 +0300

    reversed the date naming tradition

--- middlewares/write.js
+++ middlewares/write.js
@@ -7,13 +7,16 @@ const write = (data, message, user, date) => {
   const t = f.split(' ')[1].split(':');
 
   message('Creating folders...');
-  fs.mkdirSync(`dist/${user}/${d[1]}_${d[0]}_${d[2]}/${t[0]}`, { recursive: true });
+
+  const path = `dist/${user}/${d[1] + d[0] + d[2]}`;
+
+  fs.mkdirSync(`${path}/${t[0]}`, { recursive: true });
 
   message('Writing followers data to file...');
-  fs.writeFileSync(`dist/${user}/${d[1]}_${d[0]}_${d[2]}/${t[0]}/${t[1]}_followers.json`, JSON.stringify(data.followers, null, 4));
+  fs.writeFileSync(`${path}/${t[0]}/${t[1]}_followers.json`, JSON.stringify(data.followers, null, 4));
 
   message('Writing following data to file...');
-  fs.writeFileSync(`dist/${user}/${d[1]}_${d[0]}_${d[2]}/${t[0]}/${t[1]}_following.json`, JSON.stringify(data.following, null, 4));
+  fs.writeFileSync(`${path}/${t[0]}/${t[1]}_following.json`, JSON.stringify(data.following, null, 4));
 }
 
 export default write;
